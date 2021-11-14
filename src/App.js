3b269eb2eb86cd2a4ea88cdc8d5b2d7aabdc603b
Musa Opaluwa <musaopaluwa@gmail.com>
2021-11-14 23:37:53 +0100

    Counter App with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import {INCREMENT, DECREMENT} from './action'
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const islogged = useSelector (state => state.islogged);
+  const dispatch = useDispatch()
+
+  const styles = {
+    
+    wrapper: {
+      textAlign: "center",
+      background: "blue",
+      maxWidth: "500px",
+      margin: "0 auto",
+      border: "1px solid #e6e6e6",
+      padding: "40px 25px",
+      borderRadius:"5%",
+      marginTop: "50px"
+    },
+    btn: {
+      textAlign: "center",
+      justifycontent: "center",
+      borderRadius:"20%",
+      padding: "20px 25px",
+      margin: "20px"
+      },
+      button:{
+        padding:"20px"
+      },
+      
+    
+  }
+  
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+   <h1 style={styles.wrapper}>Counter {counter} </h1>
+   <div style={styles.btn} >
+   <button style={styles.button1} onClick={() => dispatch (INCREMENT())}>+</button>
+    <button style={styles.button2}  onClick={() => dispatch (DECREMENT())}>-</button>
+   </div>
+    
+    
+
+    {islogged? <h3>info i shoud not see </h3> : ''}  
+   
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, compose, createStore} from 'redux';
+import allReducer from './reducer';
+import { Provider } from 'react-redux';
+
+
+
+const store = createStore(
+  allReducer, compose(
+    applyMiddleware(),
+    window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <App/>
+  </Provider>,
+  
   document.getElementById('root')
 );
 
